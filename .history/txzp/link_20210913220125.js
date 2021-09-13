cb98474ac31da9a5a092531b37d4e6ae34685000
LHB153 <1075448489@qq.com>
2021-09-13 22:06:24 +0800

    本程序用于JavaScript的数据结构与算法

---
+++ .history/txzp/link_20210913220125.js
@@ -0,0 +1,27 @@
+function LinkedList() {
+    
+    let Node = function(element) {
+        this.element = element;   //
+        this.next = null;         //
+    };
+
+    let length = 0;
+    let head = null;
+    console.log('123')
+    this.append = function(element){
+
+        let node = new Node(element), //
+            current;
+        var a = 12312323
+            console.log('123')
+        if( head === null){
+            head = node;
+        } else {
+            current = head;
+            
+
+        }
+    }
+}
+
+LinkedList()
